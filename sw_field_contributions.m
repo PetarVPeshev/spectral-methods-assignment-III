6d4f30a2b90153dd33100e7debfb1f9890e90dcc
Petar V Peshev <40143832+PetarVPeshev@users.noreply.github.com>
2023-05-23 11:36:28 +0200

    added functions for evaluation of sw fields

---
+++ sw_field_contributions.m
@@ -0,0 +1,71 @@
+close all;
+clear;
+clc;
+
+if ~exist([pwd() '\figures'], 'dir')
+    mkdir('figures');
+end
+
+addpath('../spectral-methods-library');
+c = physconst('LightSpeed');
+
+%% PARAMETERS
+wave.f = 10 * 1e9;
+dielectric.h  = 2e-3;
+dielectric.er = 10;
+dipole.W = 0.5 * 1e-3;
+dipole.L = 15 * 1e-3;
+Nrho = 200;
+Nphi = 200;
+Nz = 201;
+
+%% DEPENDENT PARAMETERS
+wave.wavelength = c / wave.f;
+wave.k0 = 2 * pi / wave.wavelength;
+
+%% CYLINDRICAL GRID
+rho = linspace(eps, 30 * 1e-3, Nrho);
+phi = linspace(0, 2 * pi, Nphi);
+cyl_grid = meshgrid_comb(rho, phi);
+z = linspace(0, dielectric.h, Nz);
+
+%% TE1 AND TM0 PROPAGATION CONSTANTS
+[krho_te, krho_tm] = find_krho(wave.k0, wave.k0 * linspace(1, sqrt(dielectric.er), 1001), ...
+        'GroundSlab', dielectric.h, dielectric.er);
+
+%% TM WAVE VECTOR COMPONENTS IN CARTESIAN
+kx = krho_tm * cos(cyl_grid(:, :, 2));
+ky = krho_tm * sin(cyl_grid(:, :, 2));
+
+k_comp = NaN( [size(cyl_grid, 1, 2), 2] );
+k_comp(:, :, 1) = kx;
+k_comp(:, :, 2) = ky;
+
+%% RESIDUE STRATIFIED MEDIA
+[~, ~, v_tm, i_tm] = residue_stratified(wave.k0, krho_te, krho_tm, z, ...
+    'GroundSlab', dielectric.h, dielectric.er);
+
+%% DIPOLE CURRENT FOURIER TRANSFORM
+J = ft_current(wave.k0, k_comp, dipole.W, dipole.L, dielectric.er, ...
+    'dipole', 'x');
+
+%% SURFACE WAVE ELECTRIC FIELD
+E = NaN( [size(cyl_grid, 1, 2), 3, length(z)] );
+for z_idx = 1 : 1 : length(z)
+    E(:, :, :, z_idx) = sw_fields(wave.k0, krho_tm, v_tm(z_idx), ...
+        i_tm(z_idx), J, dielectric.er, cyl_grid, 'TM');
+end
+
+%% PLOT
+figure('Position', [250 250 750 400]);
+plot(rho * 1e3, real(E(1, :, 1, ceil(Nz / 2))), 'LineWidth', 2.0, ...
+    'DisplayName', '\Re\{E_{\rho}\}');
+hold on;
+plot(rho * 1e3, imag(E(1, :, 1, ceil(Nz / 2))), '--', 'LineWidth', 2.0, ...
+    'DisplayName', '\Im\{E_{\rho}\}');
+grid on;
+xlabel('\rho / mm');
+ylabel('E_{\rho}');
+title(['E_{\rho} Real & Imaginary Parts @ \phi = ' ...
+    num2str(phi(1) * 180 / pi) ' deg, and z = ' ...
+    num2str(round(z(ceil(Nz / 2)) * 1e3, 2)) ' mm']);
--- sw_propagation_constant.m
+++ sw_propagation_constant.m
@@ -12,11 +12,10 @@ c = physconst('LightSpeed');
 %% PARAMETERS
 dielectric.h  = 2e-3;
 dielectric.er = 10;
-top_medium.er = 1;
-wave.f  = linspace(1, 20, 1001) * 1e9;
 N = 1001;
 
 %% DEPENDENT PARAMETERS
+wave.f  = linspace(1, 20, N) * 1e9;
 wave.wavelength = c ./ wave.f;
 wave.k0 = 2 * pi ./ wave.wavelength;
 krho_norm = linspace(1, sqrt(dielectric.er), N);
@@ -46,28 +45,3 @@ xlabel('h / \lambda_{d}');
 ylabel('k_{\rho}/k_{0}');
 title(['Normalized k_{\rho} @ h = ' num2str(dielectric.h * 1e3) ...
     ' mm, and \epsilon_{r} = ' num2str(dielectric.er)]);
-
-%% PLOT DENOMINATOR ZEROS
-[D_te, D_tm] = dispersion_eqn(wave.k0(end), krho_norm * wave.k0(end), ...
-    'GroundSlab', dielectric.h, dielectric.er);
-
-figure('Position', [250 250 750 400]);
-yyaxis left;
-plot(krho / wave.k0(end), abs(1 ./ D_te), 'LineWidth', 2.0, ...
-    'DisplayName', 'TE');
-hold on;
-ylim([0 3]);
-ylabel('1 / D^{TE}');
-yyaxis right;
-plot(krho / wave.k0(end), abs(1 ./ D_tm), 'LineWidth', 2.0, ...
-    'DisplayName', 'TM');
-grid on;
-ylabel('1 / D^{TM}');
-ylim([0 0.1]);
-xlim([min(krho / wave.k0(end)) max(krho / wave.k0(end))]);
-legend show;
-legend('location', 'bestoutside');
-xlabel('k_{\rho} / k_{0}');
-title(['1 / D = 1 / (Z_{0}+Z_{sd}) @ f = ' num2str(wave.f(end) * 1e-9) ...
-    ' GHz, h = ' num2str(dielectric.h * 1e3) ' mm, and ' ...
-    '\epsilon_{r} = ' num2str(dielectric.er)]);
